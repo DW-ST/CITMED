df1c33dc2515ac825986a1c83424236afeecdd98
FAsaF Soporte7 <Soporte7@HLBEY.com>
2024-11-19 15:35:18 -0500

    validacion exitosa de usuarios por tipo id e id

--- server.js
+++ server.js
@@ -99,24 +99,22 @@ app.get('/pacientes/validar/:documentoPaciente', async (req, res) => {
 app.post('/pacientes', async (req, res) => {
     const { tipoID, id, nombre, telefono, direccion } = req.body;
 
-    // Verificar si todos los datos necesarios están presentes
-    if (!tipoID || !id || !nombre || !telefono || !direccion) {
+    if (!id || !nombre || !telefono || !direccion || !tipoID) {
         return res.status(400).json({ message: 'Faltan datos para agregar el paciente.' });
     }
 
     try {
         // Verificar si ya existe un paciente con el mismo tipoID e id
         const pacienteExistente = await Paciente.findOne({ tipoID, id });
-        
+
         if (pacienteExistente) {
-            return res.status(400).json({ message: 'Ya existe un paciente con este tipo de ID y número de ID.' });
+            return res.status(400).json({ message: 'Ya existe un paciente con ese tipo de ID y número de ID.' });
         }
 
-        // Crear un nuevo paciente
+        // Crear y guardar el nuevo paciente
         const paciente = new Paciente({ tipoID, id, nombre, telefono, direccion });
         await paciente.save();
         res.status(201).json(paciente);
-
     } catch (error) {
         console.error('Error al agregar paciente:', error);
         res.status(500).json({ message: 'Error al agregar paciente', error: error.message });
